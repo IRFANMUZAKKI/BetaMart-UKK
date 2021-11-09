216c151f9f22ac787ac5b201d1aa3aa777222092
IRFANMUZAKKI <irfanmuzakki9c@gmail.com>
2021-11-09 14:10:40 +0700

    first commit

---
+++ OOP/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace OOP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtkode = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtharga = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtnama = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtjenis = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.datagridview = new System.Windows.Forms.DataGridView();
+            this.btntambah = new System.Windows.Forms.Button();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbcari = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.datagridview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtkode);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.txtharga);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtnama);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtjenis);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(73, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(657, 147);
+            this.panel1.TabIndex = 43;
+            // 
+            // txtkode
+            // 
+            this.txtkode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtkode.Location = new System.Drawing.Point(155, 13);
+            this.txtkode.Name = "txtkode";
+            this.txtkode.Size = new System.Drawing.Size(228, 27);
+            this.txtkode.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(27, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 20);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "Kode Barang";
+            // 
+            // txtharga
+            // 
+            this.txtharga.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtharga.Location = new System.Drawing.Point(155, 77);
+            this.txtharga.Name = "txtharga";
+            this.txtharga.Size = new System.Drawing.Size(228, 27);
+            this.txtharga.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 20);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Harga";
+            // 
+            // txtnama
+            // 
+            this.txtnama.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnama.Location = new System.Drawing.Point(155, 46);
+            this.txtnama.Name = "txtnama";
+            this.txtnama.Size = new System.Drawing.Size(228, 27);
+            this.txtnama.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 20);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Nama Barang";
+            // 
+            // txtjenis
+            // 
+            this.txtjenis.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtjenis.FormattingEnabled = true;
+            this.txtjenis.Items.AddRange(new object[] {
+            "Makanan",
+            "Minuman",
+            "Kosmetik",
+            "Electronik"});
+            this.txtjenis.Location = new System.Drawing.Point(155, 110);
+            this.txtjenis.Name = "txtjenis";
+            this.txtjenis.Size = new System.Drawing.Size(228, 28);
+            this.txtjenis.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(27, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 20);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Jenis";
+            // 
+            // datagridview
+            // 
+            this.datagridview.AllowUserToAddRows = false;
+            this.datagridview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.datagridview.Location = new System.Drawing.Point(73, 202);
+            this.datagridview.Name = "datagridview";
+            this.datagridview.Size = new System.Drawing.Size(526, 192);
+            this.datagridview.TabIndex = 42;
+            this.datagridview.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.datagridview_CellContentClick);
+            // 
+            // btntambah
+            // 
+            this.btntambah.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btntambah.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntambah.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btntambah.Location = new System.Drawing.Point(73, 414);
+            this.btntambah.Name = "btntambah";
+            this.btntambah.Size = new System.Drawing.Size(91, 37);
+            this.btntambah.TabIndex = 35;
+            this.btntambah.Text = "Tambah";
+            this.btntambah.UseVisualStyleBackColor = false;
+            this.btntambah.Click += new System.EventHandler(this.btntambah_Click);
+            // 
+            // btnclear
+            // 
+            this.btnclear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnclear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclear.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnclear.Location = new System.Drawing.Point(508, 414);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(91, 37);
+            this.btnclear.TabIndex = 38;
+            this.btnclear.Text = "Clear";
+            this.btnclear.UseVisualStyleBackColor = false;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btndelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndelete.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btndelete.Location = new System.Drawing.Point(365, 414);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(91, 37);
+            this.btndelete.TabIndex = 37;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = false;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnupdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnupdate.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnupdate.Location = new System.Drawing.Point(216, 417);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(91, 37);
+            this.btnupdate.TabIndex = 36;
+            this.btnupdate.Text = "Edit";
+            this.btnupdate.UseVisualStyleBackColor = false;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(178, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 20);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "Cari";
+            // 
+            // tbcari
+            // 
+            this.tbcari.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbcari.Location = new System.Drawing.Point(228, 163);
+            this.tbcari.Name = "tbcari";
+            this.tbcari.Size = new System.Drawing.Size(228, 27);
+            this.tbcari.TabIndex = 28;
+            this.tbcari.TextChanged += new System.EventHandler(this.tbcari_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 466);
+            this.Controls.Add(this.tbcari);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.datagridview);
+            this.Controls.Add(this.btntambah);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnupdate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.datagridview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtkode;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtharga;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtnama;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox txtjenis;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView datagridview;
+        private System.Windows.Forms.Button btntambah;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbcari;
+    }
+}
+
---
+++ OOP/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace OOP
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=E:\OOP\dboop.accdb");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btntambah_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                OleDbCommand cmd = new OleDbCommand("Insert into tb_barang (kode_barang,nama_barang,harga,jenis) values('" + txtkode.Text + "', '" + txtnama.Text + "', '" + txtharga.Text + "', '" + txtjenis.Text + "')", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Data Berhasil Ditambahkan");
+                cmd.Dispose();
+
+                cleartext();
+                fillgrid();
+            }
+            catch (Exception)
+            { 
+                MessageBox.Show("Data Gagal Ditambahkan");
+            }
+            
+        }
+
+        void fillgrid()
+        {
+            con.Open();
+            OleDbDataAdapter da = new OleDbDataAdapter("select * from tb_barang order by ID", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            datagridview.DataSource = dt;
+            con.Close();
+        }
+
+        void cleartext()
+        {
+            txtkode.Text = "";
+            txtnama.Text = "";
+            txtharga.Text = "";
+            txtjenis.Text = "";
+        }
+
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                con.Open();
+                OleDbCommand cmd = new OleDbCommand("Update tb_barang set kode_barang='" + txtkode.Text + "', nama_barang='" + txtnama.Text + "', harga='" + txtharga.Text + "', jenis='" + txtjenis.Text + "' where kode_barang = '" + txtkode.Text + "' ", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Data Berhasil Diedit");
+                cmd.Dispose();
+
+                cleartext();
+                fillgrid();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Data Gagal Diupdatet");
+            }
+            
+
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                OleDbCommand cmd = new OleDbCommand("Delete from tb_barang where kode_barang = '" + txtkode.Text + "' ", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Data Berhasil Didelete");
+                cmd.Dispose();
+
+                cleartext();
+                fillgrid();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Data Gagal Didelete");
+            }
+        }
+
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnrefresh_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = "select * from tb_barang";
+                OleDbCommand cmd= new OleDbCommand(query, con);
+                DataSet ds = new DataSet();
+                OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+                adapter.Fill(ds, "res");
+                datagridview.DataSource = ds.Tables["res"];
+                adapter.Dispose();
+                cmd.Dispose();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Gagal Menampilkan Data");
+            }
+        }
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                OleDbCommand cmd = new OleDbCommand("Delete * from tb_barang ", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Data Berhasil Diclear");
+                cmd.Dispose();
+
+                cleartext();
+                fillgrid();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Data Gagal Diclear");
+            }
+        }
+
+        private void tbcari_TextChanged(object sender, EventArgs e)
+        {
+            con.Open();
+            string perintah = "select * from tb_barang where nama_barang='" + tbcari.Text + "'";
+            OleDbDataAdapter da = new OleDbDataAdapter(perintah, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            datagridview.DataSource = dt;
+            con.Close();
+        }
+
+        private void datagridview_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
